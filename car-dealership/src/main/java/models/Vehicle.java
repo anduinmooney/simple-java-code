763a17596139709878b5540498878ae5c7c34fef
Anduin Mooney <anduinmooney@gmail.com>
2018-01-03 15:45:24 -0800

    Add all projects of the day together.

---
+++ car-dealership/src/main/java/models/Vehicle.java
@@ -0,0 +1,26 @@
+package models;
+
+/**
+ * Created by Guest on 1/3/18.
+ */
+public class Vehicle {
+
+    public int year;
+    public String brand;
+    public String model;
+    public int miles;
+    public int price;
+
+    public Vehicle(int year, String brand, String model, int miles, int price) {
+        this.year = year;
+        this.brand = brand;
+        this.model = model;
+        this.miles = miles;
+        this.price = price;
+    }
+
+    public boolean worthBuying(int maxPrice){
+        return (this.price < maxPrice);
+    }
+
+}
